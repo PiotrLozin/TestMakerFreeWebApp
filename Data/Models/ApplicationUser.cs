9550ea65e5947e74af068c72ba95a3ea661416c4
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2024-02-02 10:01:32 +0100

    Added models to EntityFramework

---
+++ Data/Models/ApplicationUser.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestMakerFreeWebApp.Data.Models
+{
+    public class ApplicationUser
+    {
+        #region Constructor
+        public ApplicationUser()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Notes { get; set; }
+
+        [Required]
+        public int Type { get; set; }
+
+        [Required]
+        public int Flags { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+        #endregion
+    }
+}
