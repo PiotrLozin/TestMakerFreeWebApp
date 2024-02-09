44a62da33728c7993d1ebce63dfe72d444ce29f4
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2024-02-09 22:50:38 +0100

    Added quiz-edit component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { QuizListComponent } from './components/quiz/quiz-list.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { QuizEditComponent } from './components/quiz/quiz-edit.component';
 import { AboutComponent } from './components/about/about.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/pagenotfound/pagenotfound.component';
@@ -30,6 +31,7 @@ import { PageNotFoundComponent } from './components/pagenotfound/pagenotfound.co
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'quiz/create', component: QuizEditComponent },
       { path: 'quiz/:id', component: QuizComponent },
       { path: 'about', component: AboutComponent },
       { path: 'login', component: LoginComponent },
---
+++ ClientApp/src/app/components/quiz/quiz-edit.component.ts
@@ -0,0 +1,60 @@
+import { Component, Inject, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { Quiz } from "../../interfaces/quiz";
+
+@Component({
+  selector: "quiz-edit",
+  templateUrl: './quiz-edit.component.html',
+  styleUrls: ['./quiz-edit.component.css']
+})
+
+export class QuizEditComponnet {
+  title: string | undefined;
+  quiz: Quiz;
+
+  // Will receive the value true if you edit an existing quiz
+  // or false in case of new quiz
+  editMode: boolean;
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string)
+  {
+   // Create empty object interface-compatible quiz
+    this.quiz = <Quiz>{};
+
+    var id = +this.activatedRoute.snapshot.params["id"];
+    if (id) {
+      this.editMode = true;
+
+      // Upload quiz from server
+      var url = this.baseUrl + "api/quiz/" + id;
+      this.http.get<Quiz>(url).subscribe(result => {
+        this.quiz = result;
+        this.title = "Edycja - " + this.quiz.Title;
+      }, error => console.error(error));
+    }
+    else {
+      this.editMode = false;
+      this.title = "Create new quiz";
+    }
+  }
+
+  onSubmit(quiz: Quiz) {
+    var url = this.baseUrl + "api/quiz";
+
+    if (this.editMode) {
+      this.http.post<Quiz>(url, quiz).subscribe(result => {
+        var v = result;
+        console.log("Quiz " + v.Id + " was updated.");
+        this.router.navigate(["home"]);
+      }, error => console.log(error));
+    }
+  }
+
+  onBack() {
+    this.router.navigate(["home"]);
+  }
+}
